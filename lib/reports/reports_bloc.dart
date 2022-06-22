ff28535e7f91e87dc4e134cf8748e67e2a8cb1e8
rafaldziuryk <rafal.dziuryk@appvinio.com>
2022-06-22 10:23:14 +0200

    prepare to download data

--- lib/reports/reports_bloc.dart
+++ lib/reports/reports_bloc.dart
@@ -1,14 +1,17 @@
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 
 part 'reports_event.dart';
 part 'reports_state.dart';
 
 class ReportsBloc extends Bloc<ReportsEvent, ReportsState> {
-  final list = List.generate(100, (index) => index);
+  final list = [];
+  final dio = Dio(BaseOptions(baseUrl: 'https://trainingserver1.herokuapp.com/'));
 
   ReportsBloc() : super(ReportsInitial()) {
-    on<ReportsInit>((event, emit) {
+    on<ReportsInit>((event, emit) async {
+      final responseData = await dio.get('data/1');
       final dataState = ReportsData(reports: List.unmodifiable(list));
       emit(dataState);
     });
